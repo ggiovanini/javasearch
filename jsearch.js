af4db03740745584738b3f038e384b6ae31722e4
Giuliano Giovanini <gil@uparte.com.br>
2019-02-04 17:21:21 -0200

    Modify to class

--- jsearch.js
+++ jsearch.js
@@ -1,17 +1,17 @@
-function jSearch(element,value) {
+class jSearch {
+    static filter(element,value){
+        const nodeList = document.querySelectorAll(element);
+        value = value.toUpperCase();
 
-    const nodeList = document.querySelectorAll(element);
-    value = value.toUpperCase();
-
-    for (let i = 0; i < nodeList.length; i++){
-        if (nodeList[i].innerHTML.toUpperCase().indexOf(value) === -1) {
-            if( !nodeList[i].dataset.display ){
-                nodeList[i].dataset.display = nodeList[i].style.display;
+        for (let i = 0; i < nodeList.length; i++){
+            if (nodeList[i].innerHTML.toUpperCase().indexOf(value) === -1) {
+                if( !nodeList[i].dataset.display ){
+                    nodeList[i].dataset.display = nodeList[i].style.display;
+                }
+                nodeList[i].style.display = 'none';
+            }else{
+                nodeList[i].style.display = null;
             }
-            nodeList[i].style.display = 'none';
-        }else{
-            nodeList[i].style.display = null;
         }
     }
-
 }
