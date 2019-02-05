d5d830dd097e86367fed8827ab5ba89f1aae0350
Giuliano Giovanini <gil@uparte.com.br>
2019-02-05 09:22:22 -0200

    - Add function for Case Insensitive - Add function for ignore accentuation - Fix problem with return html tag elem on find

--- jsearch.js
+++ jsearch.js
@@ -1,17 +1,33 @@
 class jSearch {
-    static filter(element,value){
+    static filter(element,term)
+    {
         const nodeList = document.querySelectorAll(element);
-        value = value.toUpperCase();
+        term = this.removeSpecialChar(term);
 
         for (let i = 0; i < nodeList.length; i++){
-            if (nodeList[i].innerHTML.toUpperCase().indexOf(value) === -1) {
-                if( !nodeList[i].dataset.display ){
-                    nodeList[i].dataset.display = nodeList[i].style.display;
+            const elem = nodeList[i];
+            const origin = this.removeSpecialChar(elem.textContent || elem.innerText || '');
+            if (origin.indexOf(term) === -1) {
+                if( !elem.dataset.display ){
+                    elem.dataset.display = elem.style.display;
                 }
-                nodeList[i].style.display = 'none';
+                elem.style.display = 'none';
             }else{
-                nodeList[i].style.display = null;
+                elem.style.display = null;
             }
         }
+    }
+
+    static removeSpecialChar(term)
+    {
+        term = term.toUpperCase();
+        term = term.replace(new RegExp('[ÁÀÂÃ]','gi'), 'A');
+        term = term.replace(new RegExp('[ÉÈÊ]','gi'), 'E');
+        term = term.replace(new RegExp('[ÍÌÎ]','gi'), 'I');
+        term = term.replace(new RegExp('[ÓÒÔÕ]','gi'), 'O');
+        term = term.replace(new RegExp('[ÚÙÛ]','gi'), 'U');
+        term = term.replace(new RegExp('[Ç]','gi'), 'C');
+        term = term.replace(/[^\w\s]/gi, '');
+        return term;
     }
 }
